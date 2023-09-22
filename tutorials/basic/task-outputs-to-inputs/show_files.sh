0513df4b11030510a4de4983f680a56b22408f1c
Matthew Brown <matthew.brown@cordata.com>
2023-09-22 14:06:06 -0400

    Change output for output-to-input tutorial

--- tutorials/basic/task-outputs-to-inputs/show_files.sh
+++ tutorials/basic/task-outputs-to-inputs/show_files.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-ls some-files/*
+#ls some-files/*
+echo "some_files/file1..."
+cat some_files/file1
+echo "some_files/file2..."
+cat some_files/file2
+echo "some_files/file3..."
+cat some_files/file3
+echo "Done."
