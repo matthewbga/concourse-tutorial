36cb580308cb403c2c4367d8e122a05625b56c8b
Matthew Brown <matthew.brown@cordata.com>
2023-09-25 12:51:26 -0400

    change some_files to output_to_input_files

--- tutorials/basic/task-outputs-to-inputs/create_some_files.sh
+++ tutorials/basic/task-outputs-to-inputs/create_some_files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir output_to_input_files
+# mkdir output_to_input_files
 echo "mung-bean" > output_to_input_files/file1
 echo "file2" > output_to_input_files/file2
 echo "file3" > output_to_input_files/file3
