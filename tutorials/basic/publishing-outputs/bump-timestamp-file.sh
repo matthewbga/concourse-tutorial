0aa583f2deeb88ef89f96dace0262a85a9a5f948
Matthew Brown <matthew.brown@cordata.com>
2023-09-25 13:57:45 -0400

    Updated output for publishing-outputs

--- tutorials/basic/publishing-outputs/bump-timestamp-file.sh
+++ tutorials/basic/publishing-outputs/bump-timestamp-file.sh
@@ -7,6 +7,9 @@ git clone resource-gist updated-gist
 
 cd updated-gist
 date > bumpme
+echo "bumpme updated. Here it is..."
+cat bumpme
+echo "bumpme ---^"
 
 git config --global user.email "nobody@concourse-ci.org"
 git config --global user.name "Concourse"
